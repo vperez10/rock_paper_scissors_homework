c86dce3619477ca48334fb640d7fb603aff4bd4d
vperez10 <vperez@chicagobooth.edu>
2015-07-28 13:29:57 -0400

    Copied template from mockup
    Copied template from mockup. Working on making it functional.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,23 @@
 class GameController < ApplicationController
   # This action is for the bare domain. You can ignore it.
+  def computer_move
+  	@computer_move = ["rock", "paper", "scissors"].sample
+  end
+
   def home
     redirect_to("/mockup.html")
+  end
+
+  def rock
+  	render("play.html.erb")
+  end
+
+  def paper
+  	render("play.html.erb")
+  end
+
+  def scissors
+  	render("play.html.erb")
   end
 
   # Your code goes below.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # This route is for the bare domain. You can ignore it.
   get("/", { :controller => "game", :action => "home" })
+  get("/play/rock", { :controller => "game", :action => "rock"})
+  get("/play/paper", { :controller => "game", :action => "paper"})
+  get("/play/scissors", { :controller => "game", :action => "scissors"})
+
+  get("/flexible/:move", { :controller => "game", :action => "flexible"})
 
   # Your code goes below.
 
